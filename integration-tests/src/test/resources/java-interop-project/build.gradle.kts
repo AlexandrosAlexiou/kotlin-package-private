3674f9824caf3fcd6552ec2ff66adf6cd32c026b
Alexandros Alexiou <alex.alexiouu@gmail.com>
2026-01-05 12:42:49 +0200

    test: update integration test resources to version 1.1.0

--- integration-tests/src/test/resources/java-interop-project/build.gradle.kts
+++ integration-tests/src/test/resources/java-interop-project/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.packageprivate:package-private-annotations:1.0.0")
-    kotlinCompilerPluginClasspath("dev.packageprivate:package-private-compiler-plugin:1.0.0")
+    implementation("dev.packageprivate:package-private-annotations:1.1.0")
+    kotlinCompilerPluginClasspath("dev.packageprivate:package-private-compiler-plugin:1.1.0")
 }
